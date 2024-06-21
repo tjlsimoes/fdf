6a25398b8ae892b06689242e1764471731ff0355
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-21 13:28:19 +0100

    First steps exploring minilibx

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 
 int	main(void)
 {
+	void	*img;
 	void	*mlx;
+	void	*mlx_win;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+	img = mlx_new_image(mlx, 1920, 1080);
 }
