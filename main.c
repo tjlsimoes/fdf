9691b0e4d9d92885d0ec955ce0c26b7d988151e2
tjorge-l <tjorge-l@student.42lisboa.com>
2024-06-25 11:41:30 +0100

    Add error protection on window creation

--- main.c
+++ main.c
@@ -11,7 +11,14 @@ int	main(void)
 	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
+	if (!mlx_ptr)
+		return (1);
 	win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
+	if (!win_ptr)
+	{
+		free(mlx_ptr);	// Guide is incorrect in this step, right?
+		return (1);
+	}
 	mlx_destroy_window(mlx_ptr, win_ptr);
 	mlx_destroy_display(mlx_ptr);
 	free(mlx_ptr);
