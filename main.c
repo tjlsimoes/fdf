50ecd543cb92c07abe9cb72f0afbaf6149ff0594
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-06-25 16:55:46 +0100

    Draw rectangles && Understand flickering side-effect

--- main.c
+++ main.c
@@ -53,6 +53,169 @@
 //     return (red << 16 | green << 8 | blue);
 // }
 
+// #include "./mlx.h"
+// #include "./mlx_int.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// # define WINDOW_WIDTH 600
+// # define WINDOW_HEIGHT 300
+
+// # define RED_PIXEL 0xFF0000
+
+// typedef struct	s_data
+// {
+// 	void	*mlx_ptr;
+// 	void	*win_ptr;
+// } t_data;
+
+// int	handle_keypress(int keysym, t_data *data)
+// {
+// 	if (keysym == XK_Escape)
+// 	{
+// 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+// 		data->win_ptr = NULL;
+// 	}
+// 	return (0);
+// }
+
+// int	render(t_data *data)
+// {
+// 	// If window has been destroyed, we don't want to put the pixel!
+// 	if (data->win_ptr)
+// 		mlx_pixel_put(data->mlx_ptr, data->win_ptr, WINDOW_WIDTH / 2,  WINDOW_HEIGHT / 2, RED_PIXEL); // Guide switches width with height
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	t_data	data;
+
+// 	data.mlx_ptr = mlx_init();
+// 	if (!data.mlx_ptr)
+// 		return (1);
+// 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My Window");
+// 	if (!data.win_ptr)
+// 	{
+// 		free(data.mlx_ptr);		// Guide is again incorrect here, right?
+// 		return (1);
+// 	}
+// 	// Setup hooks
+// 	mlx_loop_hook(data.mlx_ptr, &render, &data);
+
+// 	// mlx original function seemingly not working
+// 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+// 	mlx_loop(data.mlx_ptr);
+
+// 	// Loop will be exited if there's no window left and execute following code
+// 	mlx_destroy_display(data.mlx_ptr);
+// 	free(data.mlx_ptr);
+// 	return (0);
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Drawing a rectangle
+
+// #include "./mlx.h"
+// #include "./mlx_int.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// # define WINDOW_WIDTH 600
+// # define WINDOW_HEIGHT 300
+
+// # define GREEN_PIXEL 0xFF00
+// # define RED_PIXEL 0xFF0000
+
+// typedef struct	s_data
+// {
+// 	void	*mlx_ptr;
+// 	void	*win_ptr;
+// } t_data;
+
+// typedef struct s_rect
+// {
+// 	int	x;
+// 	int y;
+// 	int width;
+// 	int height;
+// 	int color;
+// } t_rect;
+
+// // The x and y coordinates of the rectangle correspond to its
+// // left corner.
+
+// int	handle_keypress(int keysym, t_data *data)
+// {
+// 	if (keysym == XK_Escape)
+// 	{
+// 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+// 		data->win_ptr = NULL;
+// 	}
+// 	return (0);
+// }
+
+// int	render_rect(t_data *data, t_rect rect)
+// {
+// 	int	i;
+// 	int	j;
+
+// 	if (!data->win_ptr)
+// 		return (1);
+// 	i = rect.y;
+// 	while (i < rect.y + rect.height)
+// 	{
+// 		j = rect.x;
+// 		while (j < rect.x + rect.width)
+// 			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+
+// int	render(t_data * data)
+// {
+// 	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+// 	render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+// 	return (0);
+// }
+
+// // (t_rect){} is a compund literal. Since C99, this is a way to initialize structures
+// // without having to manually assign each member. Above: passing a structure by value.
+
+// int	main(void)
+// {
+// 	t_data	data;
+
+// 	data.mlx_ptr = mlx_init();
+// 	if (!data.mlx_ptr)
+// 		return (1);
+// 	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My Window");
+// 	if (!data.win_ptr)
+// 	{
+// 		free(data.mlx_ptr);		// Guide is again incorrect here, right?
+// 		return (1);
+// 	}
+// 	// Setup hooks
+// 	mlx_loop_hook(data.mlx_ptr, &render, &data);
+
+// 	// mlx original function seemingly not working
+// 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+// 	mlx_loop(data.mlx_ptr);
+
+// 	// Loop will be exited if there's no window left and execute following code
+// 	mlx_destroy_display(data.mlx_ptr);
+// 	free(data.mlx_ptr);
+// 	return (0);
+// }
+
+///////////////////////////////////////////////////////////////////////////////
+// Drawbacks of previous approach
+
 #include "./mlx.h"
 #include "./mlx_int.h"
 #include <stdlib.h>
@@ -61,13 +224,27 @@
 # define WINDOW_WIDTH 600
 # define WINDOW_HEIGHT 300
 
+# define GREEN_PIXEL 0xFF00
 # define RED_PIXEL 0xFF0000
+# define WHITE_PIXEL 0x000000
 
 typedef struct	s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
 } t_data;
+
+typedef struct s_rect
+{
+	int	x;
+	int y;
+	int width;
+	int height;
+	int color;
+} t_rect;
+
+// The x and y coordinates of the rectangle correspond to its
+// left corner.
 
 int	handle_keypress(int keysym, t_data *data)
 {
@@ -79,13 +256,52 @@ int	handle_keypress(int keysym, t_data *data)
 	return (0);
 }
 
-int	render(t_data *data)
+int	render_rect(t_data *data, t_rect rect)
 {
-	// If window has been destroyed, we don't want to put the pixel!
-	if (data->win_ptr)
-		mlx_pixel_put(data->mlx_ptr, data->win_ptr, WINDOW_WIDTH / 2,  WINDOW_HEIGHT / 2, RED_PIXEL); // Guide switches width with height
+	int	i;
+	int	j;
+
+	if (!data->win_ptr)
+		return (1);
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
+		i++;
+	}
 	return (0);
 }
+
+void 	render_background(t_data *data, int color)
+{
+	int	i;
+	int	j;
+
+	if (!data->win_ptr)
+		return ;
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+		i++;
+	}
+}
+
+int	render(t_data * data)
+{
+	render_background(data, WHITE_PIXEL);
+	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	return (0);
+}
+
+// (t_rect){} is a compund literal. Since C99, this is a way to initialize structures
+// without having to manually assign each member. Above: passing a structure by value.
+
 
 int	main(void)
 {
@@ -102,6 +318,8 @@ int	main(void)
 	}
 	// Setup hooks
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
+
+	// mlx original function seemingly not working
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 
 	mlx_loop(data.mlx_ptr);
@@ -111,4 +329,12 @@ int	main(void)
 	free(data.mlx_ptr);
 	return (0);
 }
+
+// mlx_pixel_put() basically draws the pixel on the window directly, and the person
+// who's looking at the window will see the change instantly.
+
+// What ought to happen: wait for the whole background and rectangles to be drawn
+// 						 and then push that on the window.
+
+// Everything done without delay results in a flickering effect.
 
