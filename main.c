fac120ae08361a7662397a4aab64102fca9ae078
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-07-24 10:20:55 +0100

    Add initial check for correct usage of fdf
    Note: program is outputing invalid reads when window
          is closed with a mouse-click.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 11:50:27 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/07/24 10:07:38 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/07/24 10:19:52 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,37 @@ int	destroy_window(t_fdf *env)
 	return (0);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_fdf	env;
+	char	*title;
 
-	env.mlx = mlx_init();
-	if (!env.mlx)
-		return (1);
-	env.win = mlx_new_window(env.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "FDF");
-	if (!env.win)
+	if (argc == 2)
 	{
+		env.mlx = mlx_init();
+		title = ft_strjoin(ft_strdup("FDF - "), argv[1]);
+		if (!env.mlx)
+			return (1);
+		env.win = mlx_new_window(env.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, title);
+		free(title);
+		if (!env.win)
+		{
+			mlx_destroy_display(env.mlx);
+			free(env.mlx);
+			return (1);
+		}
+
+		mlx_loop_hook(env.mlx, &handle_no_event, &env);
+		mlx_hook(env.win, KeyPress, KeyPressMask, handle_keypress, &env);
+		mlx_hook(env.win, DestroyNotify, 0, destroy_window, &env);
+		mlx_mouse_hook(env.win, handle_mouse_click, &env);
+
+		mlx_loop(env.mlx);
+
 		mlx_destroy_display(env.mlx);
 		free(env.mlx);
-		return (1);
 	}
-
-	mlx_loop_hook(env.mlx, &handle_no_event, &env);
-	mlx_hook(env.win, KeyPress, KeyPressMask, handle_keypress, &env);
-	mlx_hook(env.win, DestroyNotify, 0, destroy_window, &env);
-	mlx_mouse_hook(env.win, handle_mouse_click, &env);
-
-	mlx_loop(env.mlx);
-
-    mlx_destroy_display(env.mlx);
-    free(env.mlx);
+	else
+		ft_printf("Correct Usage: ./fdf <file_name>");
 	return (0);
 }
