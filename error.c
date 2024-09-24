a7f23e818226027a8d02a530d4b3ac122647e519
tjorge-l <tjorge-l@student.42lisboa.com>
2024-09-24 11:19:18 +0100

    Fix error function
    It wasn't calling exit to terminate the program.

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: tjorge-l <tjorge-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 13:06:31 by tjorge-l          #+#    #+#             */
-/*   Updated: 2024/09/23 11:26:39 by tjorge-l         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:13:43 by tjorge-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ void	error_close_window(t_fdf *env, char *error_msg)
 	free(env->map);
 	free(env);
 	ft_putstr_fd(error_msg, 2);
+	exit(1);
 }
